a9ea460736ffbfe77c70c042f795641721bc393c
Trey Clark <itreyclark@gmail.com>
2022-12-12 15:08:09 -0600

    Adds updated query selectors as well as a console log for the length value.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,37 @@
 lengthEl = document.getElementById("length");
 upperEl = document.getElementById("upper");
 lowerEl = document.getElementById("lower");
-numbersEl = document.getElementById("numbers")
-speicialEl = document.getElementById("special")
+numbersEl = document.getElementById("numbers");
+specialEl = document.getElementById("special");
+generateEl = document.getElementById("generate");
+passwordEl = document.getElementById("password");
+
+
+generateEl.addEventListener("click", () => {
+  var length = lengthEl.value;
+
+  console.log(length)
+});
+
+
+//Generator Functions
+
+getUpper = function(){
+
+}
+
+getLower = function(){
+
+}
+
+getNumber = function(){
+
+}
+
+
+getSpecial = function() {
+  if (specialEl === true) {
+      return ("!@#$%^&*()_+-=");
+}   else {
+      return (" ");
+}};
