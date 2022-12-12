0250fbd54ffc6c85d282df12bc06a152a2ff8027
Trey Clark <itreyclark@gmail.com>
2022-12-12 14:11:52 -0600

    Updates Input and Label tags, adds element queries on js file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,5 @@
-window.onload = function () {
-  var slider = document.getElementById("length-slider");
-  slider.addEventListener("input", function () {
-      document.body.style.setProperty("--thumbNumber", "'" + this.value + "'");
-  });
-  // whenever this element receives input, change the value of --thumbNumber to this element's value
-}
+lengthEl = document.getElementById("length");
+upperEl = document.getElementById("upper");
+lowerEl = document.getElementById("lower");
+numbersEl = document.getElementById("numbers")
+speicialEl = document.getElementById("special")
