5505d6d7241f3055ca9ad42bd6a80247a19ab48a
Trey Clark <itreyclark@gmail.com>
2022-12-13 13:49:35 -0600

    Updates to current code

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+// Dom Elements
+
 lengthEl = document.getElementById("length");
 upperEl = document.getElementById("upper");
 lowerEl = document.getElementById("lower");
@@ -11,7 +13,36 @@ passwordEl = document.getElementById("password");
 var charCodes = Array.from(Array(26)).map( (_, i) => i + 97);
 var lowerCase = charCodes.map(code => String.fromCharCode(code));
 var upperCase = lowerCase.map(lowerCase => lowerCase.toUpperCase());
-console.log(lowerCase);
-console.log(upperCase);
 var numbers = [1,2,3,4,5,6,7,8,9,0];
 var specialChar = ["!@#$%^&*().?"];
+
+// Criteria Checks
+
+var length = lengthEl.value;
+var hasUpper = upperEl.checked;
+var hasLower = lowerEl.checked;
+var hasNumbers = numbersEl.checked;
+var hasSpecial = specialEl.checked;
+
+// Generate Password 
+
+var getPassword = (length, hasLower, hasUpper, hasNumbers, hasSpecial) => {
+    var randomPassword = [
+      //Keys
+      (hasLower ? lowerCase : ""),
+      (hasUpper ? upperCase : ""),
+      (hasNumbers ? numbers : ""),
+      (hasSpecial ? specialChar : ""),
+      ];
+
+      let password = "";
+      if (randomPassword.length === 0) return "";
+
+      for (let i = 0; i < length; i++){
+        var randomIndex = math.floor(math.random() * randomPassword.length);
+        password += randomPassword[randomIndex];
+      }
+      return password;
+}
+
+generateEl.addEventListener('click', getPassword);
